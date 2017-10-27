cbdaa9e3177875783afcfd7931bc2d96ac483bbc
deepak <deepacraj616@gmail.com>
2017-10-27 15:37:19 +0530

    adding cors middleware for cross domain requests

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ var app=express();
 
 app.use(bodyParser.json());
 
+app.use(function(req,res,next){
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers','Content-Type,Authorization');
+    next();
+})
+
 app.post('/api/postMessage',function(req,res){
     console.log(req.body);
     res.status(200);
